3e60a733c65c2a070f3c78f09c40ad922cb995bd
unknown <tharushafdo2000@gmail.com>
2023-10-16 20:45:57 +0530

    Temlate Added

--- BootstrapTemplate22/webpack.mix.js
+++ BootstrapTemplate22/webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,10 +11,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/material-dashboard.css', 'public/css', [
         //
-    ]
-);
+    ]);
